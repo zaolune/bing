848ec65feec15ce2e8e090091ff9aefea24a4614
wangbeibei <wangtobei@gmail.com>
2021-04-22 21:54:05 +0800

    bing

---
+++ api/bing.py
@@ -0,0 +1,14 @@
+from http.server import BaseHTTPRequestHandler
+import requests
+
+
+class Bing(BaseHTTPRequestHandler):
+    def do_GET(self):
+        res = requests.get(
+            "https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN")
+        res = res.json()
+        imageUrl = "https://cn.bing.com"+res["images"][0]["url"]
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.wfile.write(imageUrl)
+        return

70a0df1decfafe76dcc2d2d1db543bccfd9635ca
wangbeibei <wangtobei@gmail.com>
2021-04-23 09:25:21 +0800

    bing

--- api/bing.py
+++ api/bing.py
@@ -1,5 +1,6 @@
 from http.server import BaseHTTPRequestHandler
 import requests
+import json
 
 
 class handler(BaseHTTPRequestHandler):
@@ -8,7 +9,22 @@ class handler(BaseHTTPRequestHandler):
             "https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN")
         res = res.json()
         imageUrl = "https://cn.bing.com"+res["images"][0]["url"]
+        print(imageUrl)
+        template = """
+            <!DOCTYPE html>
+            <html lang="zh-hans">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+            <body>
+            <img src="%s" alt="">
+            </body>
+            </html>
+        """ % (imageUrl)
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
-        self.wfile.write(imageUrl.encode())
+        self.wfile.write(json.dumps(template))
         return

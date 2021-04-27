117393e31c9ada6b4f0e054a565495847698da0a
wangbeibei <wangtobei@gmail.com>
2021-04-27 15:31:48 +0800

    bing

--- api/bing.py
+++ api/bing.py
@@ -18,7 +18,7 @@ class handler(BaseHTTPRequestHandler):
         return recordName
 
     def set_Record(self, json):
-        recordName = self.get_Current_FilesName
+        recordName = self.get_Current_FilesName()
         with open(join('data', recordName), 'w') as files:
             files.write(json)
         print(recordName+"saved successfully!")
@@ -26,18 +26,18 @@ class handler(BaseHTTPRequestHandler):
         return
 
     def get_Current_Record(self):
-        recordName = self.get_Current_FilesName
+        recordName = self.get_Current_FilesName()
         with open(join('data', recordName), 'w') as files:
             data = files.read()
         print("get today's record successfully!")
         return json.dumps(data)
 
     def is_saved(self):
-        recordName = self.get_Current_FilesName
+        recordName = self.get_Current_FilesName()
         return os.path.exists(join('data', recordName))
 
     def do_GET(self):
-        if not self.is_saved:
+        if not self.is_saved():
             res = requests.get(
                 "https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN")
             res = res.json()
@@ -46,7 +46,7 @@ class handler(BaseHTTPRequestHandler):
             print(imageUrl)
             return self.set_Header(imageUrl)
         else:
-            res = self.get_Current_Record
+            res = self.get_Current_Record()
             imageUrl = "https://cn.bing.com"+res["images"][0]["url"]
             print(imageUrl)
             return self.set_Header(imageUrl)
